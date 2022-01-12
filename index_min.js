769c24fb3c831752f3b740daa1bceb4726c4797a
jm12138 <2286040843@qq.com>
2022-01-12 20:29:28 +0800

    update min.js

--- index_min.js
+++ index_min.js
@@ -8,5 +8,5 @@ function addScript(url) {
 addScript('https://docs.opencv.org/4.5.5/opencv.js')
 addScript('https://cdn.jsdelivr.net/npm/onnxruntime-web@1.10.0/dist/ort.wasm.min.js')
 
-const { WebAI } = require('./src/webai')
+const WebAI = require('./src/webai')
 window.WebAI = WebAI
