cfc8b4c3d9c44ea8e0c51f8899b502e2159ae9b8
jm12138 <2286040843@qq.com>
2022-01-13 14:18:55 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const WebAI = require('./src/webai')
+const { createCanvas, loadImage } = require('canvas');
 
 WebAI.loadText = function (textURL) {
     let fs = require('fs');
@@ -14,22 +15,44 @@ WebAI.loadOpenCV = function () {
     });
 }
 
-WebAI.Model.create = async function (modelURL, inferConfig, backend = 'node', sessionOption = {logSeverityLevel: 4 }) {
-    let model = new this();
+WebAI.checkOpenCV = async function () {
     if (typeof global.cv == 'undefined') {
         await WebAI.loadOpenCV()
     }
+}
 
-    if (this.backend != backend) {
-        this.backend = backend
-        if (backend == 'web') {
-            global.ort = require('onnxruntime-web')
-        }
-        else {
-            global.ort = require('onnxruntime-node')
-        }
+WebAI.loadONNXRuntime = function (backend = 'node') {
+    WebAI.backend = backend
+    if (backend == 'web') {
+        global.ort = require('onnxruntime-web')
     }
+    else {
+        global.ort = require('onnxruntime-node')
+    }
+}
 
+WebAI.checkONNXRuntime = function (backend = 'node') {
+    if (WebAI.backend != backend) {
+        WebAI.loadONNXRuntime(backend)
+    }
+}
+
+WebAI.loadImage = async function (imageURL) {
+    WebAI.checkOpenCV()
+    let image = await loadImage(imageURL);
+    let canvas = createCanvas(image.width, image.height);
+    let ctx = canvas.getContext('2d')
+    ctx.drawImage(image, 0, 0, image.width, image.height)
+    let imageData = ctx.getImageData(0, 0, image.width, image.height)
+    let imageMat = cv.matFromArray(image.height, image.width, cv.CV_8UC4, imageData.data)
+    return imageMat
+}
+
+WebAI.Model.create = async function (modelURL, inferConfig, backend = 'node', sessionOption = { logSeverityLevel: 4 }) {
+    await WebAI.checkOpenCV();
+    WebAI.checkONNXRuntime(backend);
+
+    let model = new this();
     model.loadConfigs(inferConfig);
     model.session = await ort.InferenceSession.create(modelURL, sessionOption);
     return model
