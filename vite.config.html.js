6aac0910926975f4cfc9ad0da622a96a34a85fbb
jm12138 <2286040843@qq.com>
2022-01-26 13:54:57 +0800

    update index

--- vite.config.html.js
+++ vite.config.html.js
@@ -1,17 +1,17 @@
 export default {
-    define: {
-        BUILD: true
-    },
-    build: {
-        outDir: './docs',
-        assetsDir: './',
-        emptyOutDir: false,
-        rollupOptions: {
-          output: {
-            entryFileNames: `[name].js`,
-            chunkFileNames: `[name].js`,
-            assetFileNames: `[name].[ext]`
-          }
-        }
+  define: {
+    BUILD: true
+  },
+  build: {
+    outDir: './docs',
+    assetsDir: './',
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
     }
+  }
 }
